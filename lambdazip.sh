33fad65de37cce60bec85d3b891202e712c0b563
Andrew Jackson <jacksoa@c02rp4wufvh3.corporate.datacard.com>
2017-03-22 09:31:04 -0500

    Update echo's

--- lambdazip.sh
+++ lambdazip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-(echo "Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
-(echo "Zipping Lambda Files"; zip -r9 $zipfile ~/aws-cfn-route53backup/functions/route53_backups.py)
-(echo "Modifying cli53 shebang"; perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $VIRTUAL_ENV/bin/cli53)
-(echo "Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; zip -r9 $zipfile *)
-echo "Good or bad im done."
+(echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
+(echo "### Zipping Lambda Files"; zip -r9 $zipfile ~/aws-cfn-route53backup/functions/route53_backups.py)
+(echo "### Modifying cli53 shebang"; perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $VIRTUAL_ENV/bin/cli53)
+(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; zip -r9 $zipfile *)
+echo "### Good or bad im done. $zipfile Created."
