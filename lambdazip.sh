c83237cd58f6c4d515a0e797a55411497206e98e
Andrew Jackson <dorkmeisterx69@gmail.com>
2017-03-22 10:36:42 -0500

    add another sudo

--- lambdazip.sh
+++ lambdazip.sh
@@ -2,5 +2,5 @@
 (echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
 (echo "### Zipping Lambda Files"; sudo mkdir $HOME/upload; sudo zip -r9 $zipfile ~/aws-cfn-route53backup/functions/route53_backups.py)
 (echo "### Modifying cli53 shebang"; perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $VIRTUAL_ENV/bin/cli53)
-(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; zip -r9 $zipfile *)
+(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
 echo "### Good or bad im done. $zipfile Created."
