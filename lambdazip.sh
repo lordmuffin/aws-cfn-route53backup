6ef42498df098bff0a837cce9db3f01b59f5ba4a
Andrew Jackson <andrew.jackson@entrustdatacard.com>
2017-03-28 15:14:10 -0500

    download cli53 and add to bin

--- lambdazip.sh
+++ lambdazip.sh
@@ -2,10 +2,9 @@
 (echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
 (echo "### Find AWSCLI"; out="$(which aws)"; echo $out)
 (echo "### Make Zip Folder"; sudo mkdir $HOME/upload; )
-(echo "### Install cli53"; sudo ls -lra $GOHOME)
-(echo "### Bin FOlder "; sudo ls -lra $HOME/bin)
-#(echo "### Copying AWSCLI"; sudo rsync -va "$VIRTUAL_ENV/bin/aws" "$HOME/aws/"; sudo perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $HOME/aws/aws)
-#(echo "### Zipping AWSCLI"; cd $HOME/aws; sudo zip -r9 $zipfile *)
-#(echo "### Zipping Lambda Files";cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
-#(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
-#echo "### Good or bad im done. $zipfile Created."
+(echo "### Install cli53"; sudo wget https://github.com/barnybug/cli53/releases/download/0.8.7/cli53-linux-386; sudo chmod +x $HOME/cli53-linux-386; sudo zip -r9 $zipfile cli53-linux-386)
+(echo "### Copying AWSCLI"; sudo rsync -va "$VIRTUAL_ENV/bin/aws" "$HOME/aws/"; sudo perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $HOME/aws/aws)
+(echo "### Zipping AWSCLI"; cd $HOME/aws; sudo zip -r9 $zipfile *)
+(echo "### Zipping Lambda Files";cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
+(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
+echo "### Good or bad im done. $zipfile Created."
