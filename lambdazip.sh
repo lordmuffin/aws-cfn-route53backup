3223d107c7a5ce2b9a17ce43ba75d81f0b09eb44
Andrew Jackson <andrew.jackson@entrustdatacard.com>
2017-03-28 13:45:54 -0500

    New var

--- lambdazip.sh
+++ lambdazip.sh
@@ -2,7 +2,7 @@
 (echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
 (echo "### Find AWSCLI"; out="$(which aws)"; echo $out)
 (echo "### Copying AWSCLI"; sudo rsync -va "$VIRTUAL_ENV/bin/aws" "$HOME/"; perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' ~/aws)
-(echo "### Zipping AWSCLI";  cd $HOME; sudo zip -r9 $zipfile ./aws)
+(echo "### Zipping AWSCLI"; sudo zip -r9 $zipfile $HOME/aws)
 (echo "### Zipping Lambda Files"; sudo mkdir $HOME/upload; cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
 (echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
 echo "### Good or bad im done. $zipfile Created."
