6537f773205c88621276a4aa1af88677b7b9689f
Andrew Jackson <andrew.jackson@entrustdatacard.com>
2017-03-28 15:03:30 -0500

    update

--- lambdazip.sh
+++ lambdazip.sh
@@ -3,8 +3,9 @@
 (echo "### Find AWSCLI"; out="$(which aws)"; echo $out)
 (echo "### Make Zip Folder"; sudo mkdir $HOME/upload; )
 (echo "### Install cli53"; sudo ls -lra $GOHOME)
-(echo "### Copying AWSCLI"; sudo rsync -va "$VIRTUAL_ENV/bin/aws" "$HOME/aws/"; sudo perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $HOME/aws/aws)
-(echo "### Zipping AWSCLI"; cd $HOME/aws; sudo zip -r9 $zipfile *)
-(echo "### Zipping Lambda Files";cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
-(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
-echo "### Good or bad im done. $zipfile Created."
+(echo "### Bin FOlder "; sudo ls -lra $HOME/bin)
+#(echo "### Copying AWSCLI"; sudo rsync -va "$VIRTUAL_ENV/bin/aws" "$HOME/aws/"; sudo perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $HOME/aws/aws)
+#(echo "### Zipping AWSCLI"; cd $HOME/aws; sudo zip -r9 $zipfile *)
+#(echo "### Zipping Lambda Files";cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
+#(echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
+#echo "### Good or bad im done. $zipfile Created."
