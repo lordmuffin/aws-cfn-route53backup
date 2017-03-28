798bfe2b025b650dc285d22808a54a1374d36474
Andrew Jackson <andrew.jackson@entrustdatacard.com>
2017-03-28 14:57:29 -0500

    update ls -lra

--- lambdazip.sh
+++ lambdazip.sh
@@ -2,7 +2,7 @@
 (echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
 (echo "### Find AWSCLI"; out="$(which aws)"; echo $out)
 (echo "### Make Zip Folder"; sudo mkdir $HOME/upload; )
-(echo "### Install cli53"; lsa -lr $HOME)
+(echo "### Install cli53"; ls -lra $HOME)
 (echo "### Copying AWSCLI"; sudo rsync -va "$VIRTUAL_ENV/bin/aws" "$HOME/aws/"; sudo perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' $HOME/aws/aws)
 (echo "### Zipping AWSCLI"; cd $HOME/aws; sudo zip -r9 $zipfile *)
 (echo "### Zipping Lambda Files";cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
