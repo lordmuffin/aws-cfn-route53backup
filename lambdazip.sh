dbb757bdf556a7d9b4d4ba0b5953b00273ab4ed1
Andrew Jackson <andrew.jackson@entrustdatacard.com>
2017-03-28 13:08:50 -0500

    update rsync syntax

--- lambdazip.sh
+++ lambdazip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 (echo "### Running Git Clone"; git clone $CIRCLE_REPOSITORY_URL ~/aws-cfn-route53backup)
 (echo "### Find AWSCLI"; out="$(which aws)"; echo $out)
-(echo "### Zipping AWSCLI"; sudo rsync -va "$out" ~/; perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' ~/aws; cd ~/; sudo zip -r9 $zipfile aws)
+(echo "### Zipping AWSCLI"; sudo rsync -va "$out" "~/"; perl -pi -e '$_ = "#!/usr/bin/python\n" if $. == 1' ~/aws; cd ~/; sudo zip -r9 $zipfile aws)
+(echo "### echo aws"; ls ~/)
 (echo "### Zipping Lambda Files"; sudo mkdir $HOME/upload; cd ~/aws-cfn-route53backup/functions/; sudo zip -r9 $zipfile *)
 (echo "### Zipping Site-Packages"; cd $VIRTUAL_ENV/lib/python2.7/site-packages; sudo zip -r9 $zipfile *)
 echo "### Good or bad im done. $zipfile Created."
