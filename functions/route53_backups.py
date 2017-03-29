834c29c74d4d92a96cc6d39b05e0ce87cccb5de2
Andrew Jackson <andrew.jackson@entrustdatacard.com>
2017-03-29 12:21:28 -0500

    Update with shlex function

--- functions/route53_backups.py
+++ functions/route53_backups.py
@@ -2,12 +2,32 @@
 # Written by: Andrew Jackson
 # This is used to pull repo from github and drop to S3
 import boto3
-import subprocess
 import json
 import cfnresponse
+from subprocess import Popen, PIPE
 import shlex
 s3 = boto3.resource('s3')
 
+def run(cmd):
+  """Runs the given command locally and returns the output, err and exit_code."""
+  if "|" in cmd:
+    cmd_parts = cmd.split('|')
+  else:
+    cmd_parts = []
+    cmd_parts.append(cmd)
+  i = 0
+  p = {}
+  for cmd_part in cmd_parts:
+    cmd_part = cmd_part.strip()
+    if i == 0:
+      p[i]=Popen(shlex.split(cmd_part),stdin=None, stdout=PIPE, stderr=PIPE)
+    else:
+      p[i]=Popen(shlex.split(cmd_part),stdin=p[i-1].stdout, stdout=PIPE, stderr=PIPE)
+    i = i +1
+  (output, err) = p[i-1].communicate()
+  exit_code = p[0].wait()
+
+  return str(output), str(err), exit_code
 
 def newSplit(value):
     lex = shlex.shlex(value)
@@ -19,10 +39,16 @@ def newSplit(value):
 def lambda_handler(event, context):
     print "event.dump = " + json.dumps(event)
     command = event["command"]
-    args = shlex.split(command)
-    print args
-    print(subprocess.check_output(args, stderr=subprocess.STDOUT))
-
+    output, err, exit_code = run(command)
+    if exit_code != 0:
+      print "Output:"
+      print output
+      print "Error:"
+      print err
+      # Handle error here
+    else:
+      # Be happy :D
+      print output
 
 #import subprocess
 #command = ["./cli53", "list"]
