b2274fba2a5d0ab6d82fa3670b72b62a9762f5ef
Andrew Jackson <dorkmeisterx69@gmail.com>
2017-05-02 18:16:30 -0500

    Updates

--- functions/lambdacli.py
+++ functions/lambdacli.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 # Written by: Andrew Jackson
-# This is used to dump a specific zone to a specified s3 bucket.
-
+# This is used to pull repo from github and drop to S3
 import boto3
 import json
 import cfnresponse
 from subprocess import Popen, PIPE
-from datetime import datetime
 import shlex
 s3 = boto3.resource('s3')
 
@@ -40,9 +38,8 @@ def newSplit(value):
 
 def lambda_handler(event, context):
     print "event.dump = " + json.dumps(event)
-    command = "./cli53 export --full " + event["detail"]["requestParameters"]["hostedZoneId"]
-    filename = event["detail"]["requestParameters"]["hostedZoneId"] + datetime.now().isoformat()
-    bucket_name = "prod-route53-backups"
+    command = event["command"]
+    filename = event["hostedZoneId"]
     output, err, exit_code = run(command)
     if exit_code != 0:
         print "Output:"
@@ -55,4 +52,26 @@ def lambda_handler(event, context):
         print "Output:"
         print output
         print "Upload to S3:"
-        s3.Bucket(bucket_name).put_object(Key=filename, Body=output)
+        s3.Bucket(event["bucket_name"]).put_object(Key=filename, Body=output)
+        #cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, ".zip pulled to S3 Bucket!")
+
+
+#import subprocess
+#command = ["./cli53", "list"]
+#print(subprocess.check_output(command, stderr=subprocess.STDOUT))
+#
+
+#  cli53:
+#    branch: master
+#    commands:
+#      - cli53 list | grep 'Name:*' | cut -f6- -d' ' | while read line; do cli53 export ${line} >> backup/${line}bk; done
+#  s3up:
+#    branch: master
+#    commands:
+#      - s3up -source=backup/ -bucket=$BUCKET
+
+
+#https://github.com/barnybug/cli53/releases/download/0.8.7/cli53-linux-amd64
+#$ sudo mv cli53-my-platform /usr/local/bin/cli53
+#$ sudo chmod +x /usr/local/bin/cli53
+#
